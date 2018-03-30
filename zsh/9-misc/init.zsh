7ecb7bb4deadf9685af9f789af751f54ae3d077b
Romain TRUCHI <romain.truchi.06@gmail.com>
2018-03-30 13:34:25 +0200

    zsh: command not found (debian/arch)

--- zsh/9-misc/init.zsh
+++ zsh/9-misc/init.zsh
@@ -1,4 +1,15 @@
 #
 # Misc
 #
+
+# Options
 setopt   INTERACTIVE_COMMENTS           # Allows comments in interactive shells
+
+# Command not found
+if [[ -s '/etc/zsh_command_not_found' ]] # Debia-based
+then
+  source '/etc/zsh_command_not_found'
+elif [[ -s '/usr/share/doc/pkgfile/command-not-found.zsh' ]] # Arch-based
+then
+  source '/usr/share/doc/pkgfile/command-not-found.zsh'
+fi
